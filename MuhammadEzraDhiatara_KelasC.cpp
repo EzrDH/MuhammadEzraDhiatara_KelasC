9964ec3fbf5fb9c2bf1cdd06dfaae52d887c3157
EzrDH <112565506+EzrDH@users.noreply.github.com>
2022-09-08 13:45:57 +0700

    Add files via upload

---
+++ MuhammadEzraDhiatara_KelasC.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+	
+	cout<<"Masukkan angka: ";
+	cin>>a;
+	cout<<"Masukkan angka lagi: ";
+	cin>>b;
+	
+	cout<<a+b<<endl;
+	cout<<a-b<<endl;
+	cout<<a*b<<endl;
+	cout<<a/b<<endl;
+	cout<<a%b<<endl;
+	
+	return 0;
+}
+
